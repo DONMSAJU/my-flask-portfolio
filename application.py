5b762fa743ad208e7efe5ac9c2d2229df4d5e8c8
Don M S <donsaju2000@gmail.com>
2025-08-16 19:55:14 -0400

    Update Flask app for Elastic Beanstalk deployment

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-application = app
+application = app  # Beanstalk looks for "application"
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    # Bind to 0.0.0.0 and port 8080 for Elastic Beanstalk
+    application.run(host='0.0.0.0', port=8080)
